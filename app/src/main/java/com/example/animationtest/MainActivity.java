4e976cca0817254ad094ad6ffc8033aa86869612
Abel <abelvanbeek@hotmail.com>
2020-10-21 17:25:11 +0200

    Dynamic animations using data binding

--- app/src/main/java/com/example/animationtest/MainActivity.java
+++ app/src/main/java/com/example/animationtest/MainActivity.java
@@ -1,14 +1,68 @@
 package com.example.animationtest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.codyzen.spriterunner.SpriteView;
+import com.example.animationtest.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    public Animation animation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        animation = new Animation( this, "avatar_climb_4", 4,8);
+        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        binding.setAnimation(animation);
+
+        // Temporary cod for changing animation
+//        new android.os.Handler().postDelayed(
+//            new Runnable() {
+//                public void run() {
+//                    animation.updateAnimation("avatar_idle_4");
+//                    binding.setAnimation(animation);
+//                }
+//            },
+//            2000);
+    }
+
+    @BindingAdapter("src")
+    public static void setSrc(SpriteView view, Drawable image) {
+        Bitmap bm = drawableToBitmap(image);
+        view.setImage(bm);
+        view.start();
+    }
+
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        Bitmap bitmap = null;
+
+        if (drawable instanceof BitmapDrawable) {
+            BitmapDrawable bitmapDrawable = (BitmapDrawable) drawable;
+            if(bitmapDrawable.getBitmap() != null) {
+                return bitmapDrawable.getBitmap();
+            }
+        }
+
+        if(drawable.getIntrinsicWidth() <= 0 || drawable.getIntrinsicHeight() <= 0) {
+            bitmap = Bitmap.createBitmap(1, 1, Bitmap.Config.ARGB_8888); // Single color bitmap will be created of 1x1 pixel
+        } else {
+            bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Bitmap.Config.ARGB_8888);
+        }
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+        return bitmap;
     }
 }
