6c38467a7c82f963ff7760403a51daf1f76f4bf9
Abel <abelvanbeek@hotmail.com>
2020-10-23 15:53:47 +0200

    Its running and doing something...

--- app/src/main/java/com/example/animationtest/MainActivity.java
+++ app/src/main/java/com/example/animationtest/MainActivity.java
@@ -4,26 +4,48 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.BindingAdapter;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.util.Log;
 
 import com.codyzen.spriterunner.SpriteView;
 import com.example.animationtest.databinding.ActivityMainBinding;
+import com.example.animationtest.databinding.BubbleAvatarBinding;
 
 public class MainActivity extends AppCompatActivity {
-
     public Animation animation;
+    public final static int PERMISSION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        animation = new Animation( this, "avatar_climb_4", 4,8);
-        final ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+
+        final BubbleAvatarBinding binding = DataBindingUtil.inflate(getLayoutInflater(), R.layout.bubble_avatar, null, false);
+        setContentView(binding.getRoot());
+
+        //Check permission (allow if this application is allowed to draw other applications)
+        //Default for API < 23
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M
+                && !Settings.canDrawOverlays(this)) {
+            //Grant permission if the draw over permission is not available
+            Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                    Uri.parse("package:" + getPackageName()));
+
+            startActivityForResult(intent, PERMISSION_REQUEST_CODE);
+        } else {
+            showBubbleAvatar();
+        }
+
+        animation = new Animation(this, "avatar_climb_4", 4,8);
         binding.setAnimation(animation);
 
         // Temporary cod for changing animation
@@ -35,6 +57,23 @@ public class MainActivity extends AppCompatActivity {
 //                }
 //            },
 //            2000);
+    }
+
+    //Activate the bubble avatar if the permission is allowed
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (requestCode == RESULT_OK) {
+                showBubbleAvatar();
+            }
+        }
+    }
+
+    //Display the bubble avatar
+    public void showBubbleAvatar() {
+        startService(new Intent(MainActivity.this, BubbleAvatarService.class));
     }
 
     @BindingAdapter("src")
